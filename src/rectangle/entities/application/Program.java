973df3651ee2302ce30017aa82ef2122f11eb4e5
peuhkk <ph6353752@gmail.com>
2026-03-12 22:38:13 -0300

    Initial commit

---
+++ src/rectangle/entities/application/Program.java
@@ -0,0 +1,24 @@
+package rectangle.entities.application;
+
+import rectangle.entities.Rectangle;
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rectangle rectangle = new Rectangle();
+
+        System.out.println("Enter rectangle width and height: ");
+        rectangle.width = sc.nextDouble();
+        rectangle.height = sc.nextDouble();
+
+        System.out.println("AREA = " +rectangle.area());
+        System.out.println("PERIMETER = " +rectangle.perimeter());
+        System.out.println("DIAGONAL = " +rectangle.diagonal());
+
+    }
+}
