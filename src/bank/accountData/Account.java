12d9b326e09e0016bcf85866aad759c7c426fe6a
peuhkk <ph6353752@gmail.com>
2026-03-16 22:55:18 -0300

    Adicionei as classes Account e Balance, porém até o momento eu configurei apenas a classe Account. Utilizei dos Construtores

---
+++ src/bank/accountData/Account.java
@@ -0,0 +1,17 @@
+package bank.accountData;
+
+public class Account {
+
+    public static int accountNumber;
+    public static String accountHolder;
+
+    //Construtores
+    public Account(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public Account(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+}
